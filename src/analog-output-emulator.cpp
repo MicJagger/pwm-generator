05fb1198019fe360a6b84d3d2194c8191d73ea77
Michael Jagiello <michaeljr623@hotmail.com>
2025-03-12 17:40:14 -0400

    major new settings for better regulation, RunLoop()

--- src/analog-output-emulator.cpp
+++ src/analog-output-emulator.cpp
@@ -1,45 +1,112 @@
 #include "analog-output-emulator.hpp"
 
+AnalogOutputEmulator::AnalogOutputEmulator() {
+  _cycleCount = 4;
+  _cycleLengthExtension = 8;
+  _queued = false;
+  _skewHIGH = true;
+}
+
 void AnalogOutputEmulator::SetVoltage(uint8_t pin, uint8_t value) {
-  insertionQueue.emplace(pin, value);
+  _mutex.lock();
+  _insertionQueue.emplace(pin, value);
+  _queued = true;
+  _mutex.unlock();
+}
+
+void AnalogOutputEmulator::SetCycleCount(uint16_t cycleCount) {
+  _mutex.lock();
+  _cycleCount = cycleCount;
+  _mutex.unlock();
+}
+
+void AnalogOutputEmulator::SetCycleLengthExtension(uint16_t cycleLengthExtension) {
+  _mutex.lock();
+  _cycleLengthExtension = cycleLengthExtension;
+  _mutex.unlock();
+}
+
+void AnalogOutputEmulator::SetSkew(bool skewHIGH) {
+  _mutex.lock();
+  _skewHIGH = skewHIGH;
+  _mutex.unlock();
 }
 
 void AnalogOutputEmulator::Run() {
-  // insert new values
-  if (!insertionQueue.empty()) {
-    mtx.lock();
-    for (auto iter = insertionQueue.begin(); iter != insertionQueue.end(); iter++) {
-      uint8_t pin = iter->first;
-      uint8_t value = iter->second;
-      if (analogPins.find(pin) != analogPins.end()) {
-        if (value == 0) {
-          analogPins.erase(iter->first);
-        }
-        analogPins[pin] = value;
-      }
-      else if (value > 0) {
-        analogPins.emplace(pin, value);
-      }
-    }
-    insertionQueue.clear();
-    mtx.unlock();
-  }
+  _mutex.lock();
 
-  // run i-- pulse loop
-  for (int i = 254; i >= 0; i--) {
-  //for (int i = 0; i < 255; i++) {
-    for (auto iter = analogPins.begin(); iter != analogPins.end(); iter++) {
-      uint8_t pin = iter->first;
-      uint8_t percentage = iter->second;
-      if (percentage > i) {
-        digitalWrite(pin, HIGH);
+  if (_queued) {
+    uint8_t pin;
+    uint8_t value;
+    for (auto iter = _insertionQueue.begin(); iter != _insertionQueue.end(); iter++) {
+      pin = iter->first;
+      value = iter->second;
+      if (value == 0) {
+        _analogPins.erase(pin);
+        digitalWrite(pin, 0);
+      }
+      else if (_analogPins.find(pin) != _analogPins.end()) {
+        _analogPins[pin] = value;
       }
       else {
-        digitalWrite(pin, LOW);
+        _analogPins.emplace(pin, value);
       }
     }
+    _insertionQueue.clear();
+    _queued = false;
+  }
+  
+  _mutex.unlock();
 
-    // ensures long enough pulse for stability at all ranges
-    delayMicroseconds(1);
+  // skewed up (default, usually closer to accurate)
+  if (_skewHIGH) {
+    for (int cycle = 0; cycle < _cycleCount; cycle++) {
+
+      // run i-- pulse loop
+      for (int i = 254; i >= 0; i--) {
+          
+        for (auto iter = _analogPins.begin(); iter != _analogPins.end(); iter++) {
+          uint8_t pin = iter->first;
+          uint8_t value = iter->second;
+          if (value > i) {
+            digitalWrite(pin, HIGH);
+          }
+          else {
+            digitalWrite(pin, LOW);
+          }
+        }
+
+        delayMicroseconds(_cycleLengthExtension);
+      }
+    }
+  }
+
+  // skewed down (non-default, less likely to be correct voltage)
+  else {
+    for (int cycle = 0; cycle < _cycleCount; cycle++) {
+
+      // run i++ pulse loop
+      for (int i = 0; i < 255; i++) {
+
+        for (auto iter = _analogPins.begin(); iter != _analogPins.end(); iter++) {
+          uint8_t pin = iter->first;
+          uint8_t value = iter->second;
+          if (value > i) {
+            digitalWrite(pin, HIGH);
+          }
+          else {
+            digitalWrite(pin, LOW);
+          }
+        }//*/
+
+        delayMicroseconds(_cycleLengthExtension);
+      }
+    }
+  }//*/
+}
+
+void AnalogOutputEmulator::RunLoop() {
+  while (true) {
+    Run();
   }
 }
--- src/analog-output-emulator.hpp
+++ src/analog-output-emulator.hpp
@@ -7,17 +7,66 @@
 // This should be ran mostly on its own, as it is timing sensitive
 // Recommended to run alone in loop() and have (nearly) everything else run on a separate core
 class AnalogOutputEmulator {
-  // pin number, voltage value
-  std::map<uint8_t, uint8_t> analogPins;
-  // pin number, voltage value
-  std::map<uint8_t, uint8_t> insertionQueue;
 
-  std::mutex mtx;
+  // pin number, voltage value
+  std::map<uint8_t, uint8_t> _analogPins;
+
+  // 
+  std::map<uint8_t, uint8_t> _insertionQueue;
+  bool _queued;
+
+  // 
+  std::mutex _mutex;
+
+  // default = 1, setting to 0 disables PWMs but not queuing updates
+  uint16_t _cycleCount;
+
+  // extends the length of the each pulse by n microseconds
+  // useful for having a longer cycle, which fights voltage inaccuracy if the thread has other work to do
+  // default = 1
+  uint16_t _cycleLengthExtension;
+
+  // default = true
+  bool _skewHIGH;
+
 public:
+
+  AnalogOutputEmulator();
+
+  // Settings
+
+  // controls how many cycles are done in each execution of Run()
+  // increases the execution time of Run() roughly linearly 
+  // most useful to ensure more stable voltages if needed for some reason
+  // CAUTION: MIXING HIGH CYCLE LENGTH WITH CYCLE COUNT MAY CRASH YOUR MICROCONTROLLER DUE TO WATCHDOG
+  // default = 4
+  void SetCycleCount(uint16_t cycleCount);
+
+  // extends the length of a pulse period
+  // increases the execution time of Run() roughly linearly
+  // If this is set too high, the pulses may get too long and you may experience improper behavior (such as LED flickering)
+  // CAUTION: MIXING HIGH CYCLE LENGTH WITH CYCLE COUNT MAY CRASH YOUR MICROCONTROLLER DUE TO WATCHDOG
+  // default = 8
+  void SetCycleLengthExtension(uint16_t cycleLengthExtension);
+
+  // Inbetween one pulse period, pulses are normally left on HIGH
+  // this setting will leave pulses on LOW between Run() runs if you so want to do that
+  // default = true
+  void SetSkew(bool skewHIGH);
+
+
+  // Main
+
   // ensure pin is set to output before doing this
   // value is 0-255, with 0 being 0 V and 255 being Vout (in my case, ~3.28 V)
   void SetVoltage(uint8_t pin, uint8_t value);
 
   // put this in main loop() function, and dedicate everything (or essentially everything else) to tasks on the other core(s)
+  // this function is time sensitive to the proper voltage settings on the pins
+  // if there is a reasonable amount of other processing to do on this core, consider changing cycleCount or cycleLengthExtension
   void Run();
+
+  // runs the Run() function in an indefinite while loop
+  // Be careful using this, as it could trigger a watchdog reset (esp32, do not on core 0)
+  void RunLoop();
 };
